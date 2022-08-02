6d70b408f8a7c95a7ab2ac0e6e35a56ce2a49ee2
Krishna patel <krishanapatel17@gmail.com>
2022-08-02 13:01:22 +0530

    final changes

--- Form.js
+++ Form.js
@@ -1,14 +1,26 @@
 function handlesubmit(){
-        
 
- 
+    let firstName = document.getElementById("inputfirstname").value.trim(); 
+    let lastName = document.getElementById("inputlastname").value.trim();
+    let email = document.getElementById("inputemail").value.trim();
+    let password = document.getElementById("inputpassword").value.trim();
+    let confirmpassword = document.getElementById("inputconfirmpassword").value.trim();
+    let phonenumber = document.getElementById("inputphonenumber").value.trim();
+    let male = document.getElementById("inputgendermale").checked;
+    let female = document.getElementById("inputgenderfemale").checked;
+
+// REGULAR EXPRESSION
+    
+    let nameREGX = /^[A-Za-z]{2,}$/;
+    let emailREGX = /^[A-Za-z0-9+_.-]+@[A-Za-z]+\.[a-zA-Z]{2,4}$/
+    let passwordREGX =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    let phonenumberREGX = /^[7/8/9]+[0-9]{09}$/
+
 // VALIDATION FOR FIRSTNAME
-var firstName = document.getElementById("inputfirstname").value;
-var nameREGX = /^[A-Za-z]{2,}$/;
+
 
 if (firstName == null || firstName == undefined  ||firstName == ""){
     document.getElementById("firstnameerr").innerText="required firstname";
-    document.getElementById("firstnameerr").style.color ="red";
 }
 else if(nameREGX.test(firstName)){
     document.getElementById("firstnameerr").innerHTML =" ";
@@ -16,17 +28,13 @@ else if(nameREGX.test(firstName)){
 }
 else{
     document.getElementById("firstnameerr").innerHTML ="please enter valid firstname";
-    document.getElementById("firstnameerr").style.color = "red";
     return false;
 }
 
 // VALIDATION FOR LASTNAME
-var lastName = document.getElementById("inputlastname").value;
-var nameREGX = /^[A-Za-z]{2,}$/;
 
 if(lastName == null ||  lastName == undefined || lastName == ""){
     document.getElementById("lastnameerr").innerText="required lastname";
-    document.getElementById("lastnameerr").style.color ="red";
 }
 
 else if(nameREGX.test(lastName)){
@@ -34,88 +42,87 @@ else if(nameREGX.test(lastName)){
 }
 else{
     document.getElementById("lastnameerr").innerHTML ="please enter valid lastname";
-    document.getElementById("lastnameerr").style.color = "red";
     return false;
 } 
 
 // VALIDATION FOR EMAIL
-var email = document.getElementById("inputemail").value;
-var emailREGX = /^[A-Za-z0-9+_.-]+@[A-Za-z]+\.[a-zA-Z]{2,4}$/
 
 if(email == null ||  email == undefined || email == ""){
-    document.getElementById("emailerr").innerText="required email";
-    document.getElementById("emailerr").style.color ="red";
+    document.getElementById("emailerr").innerHTML="required email";
 }
 
 else if(emailREGX.test(email)){
-    document.getElementById("emailerr").innerText = " ";
+    document.getElementById("emailerr").innerHTML = " ";
 }
 else{
     document.getElementById("emailerr").innerHTML ="please enter valid email";
-    document.getElementById("emailerr").style.color = "red";
     return false;
 } 
 
 // VALIDATION FOR PASSWORD
-var password = document.getElementById("inputpassword").value;
-var passwordREGX =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 
+let passspecialcharREGX = /[@$!%*?&]{1,}/ ;
+let passNumREGX = /[0-9]{1,}/
+let passCharREGX = /[a-zA-Z]{0,}/
 
 if(password == null ||  password == undefined || password == ""){
-    document.getElementById("passworderr").innerText="required password";
-    document.getElementById("passworderr").style.color ="red";
-}
+    document.getElementById("passworderr").innerHTML="<li>required min 8 character</li> <li>atleast one upprcase & lowercase</li> <li>atleast one number</li> </li> <li>atleast one special character(@$!%*?&)</li>";
 
-else if(passwordREGX.test(password)){
-    document.getElementById("passworderr").innerText = " ";
 }
 else{
-    document.getElementById("passworderr").innerHTML ="please enter valid password";
-    document.getElementById("passworderr").style.color = "red";
-    return false;
+    let err = "";
+ 
+if(!passCharREGX.test(password))
+    err += "<li>atleast one upprcase & lowercase</li>"
+
+ if(!passNumREGX.test(password))
+    err += "<li>atleast one number</li>"
+
+
+ if(!passspecialcharREGX.test(password))
+    err += " <li>atleast one special character(@$!%*?&)</li>"
+
+ if(!passwordREGX.test(password))
+    err += "<li>min 8 character</li>"
+    document.getElementById("passworderr").innerHTML= err;
+   
 }
 
 // VALIDATION FOR CONFIRM-PASSWORD
-var confirmpassword = document.getElementById("inputconfirmpassword").value;
-var passwordREGX =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 
 
 if(confirmpassword == null ||  confirmpassword == undefined || confirmpassword == ""){
-    document.getElementById("confirmpassworderr").innerText="required confirm-password";
-    document.getElementById("confirmpassworderr").style.color ="red";
+    document.getElementById("confirmpassworderr").innerHTML = "required confirm-password";
 }
 
-else if(passwordREGX.test(password) && password == confirmpassword){
-    document.getElementById("confirmpassworderr").innerText = " ";
+else if(passwordREGX.test(confirmpassword) && password == confirmpassword){
+    document.getElementById("confirmpassworderr").innerHTML = "";
 }
+
 else{
-    document.getElementById("confirmpassworderr").innerHTML ="password & confirm-password not match";
-    document.getElementById("confirmpassworderr").style.color = "red";
+   document.getElementById("confirmpassworderr").innerHTML ="password & confirm-password not match";
+    console.log(confirmpassword);
+
     return false;
 }
 
 // VALIDATION FOR PHONE-NUMBER
-var phonenumber = document.getElementById("inputphonenumber").value;
-var phonenumberREGX = /^[7/8/9]+[0-9]{09}$/
 
 
 if(phonenumber == null ||  phonenumber == undefined || phonenumber == ""){
     document.getElementById("phonenumbererr").innerText="required phone-number";
-    document.getElementById("phonenumbererr").style.color ="red";
 }
 
 else if(phonenumberREGX.test(phonenumber)){
     document.getElementById("phonenumbererr").innerText = " ";
 }
 else{
-    document.getElementById("phonenumbererr").innerHTML ="please enter phonenumber";
-    document.getElementById("phonenumbererr").style.color = "red";
+    document.getElementById("phonenumbererr").innerHTML ="please enter min 10 digits phonenumber";
     return false;
 }
 
 // VALIDATION FOR GENDER
-var male = document.getElementById("inputgendermale").checked;
-var female = document.getElementById("inputgenderfemale").checked;
+
 
 if((male)){
     document.getElementById("gendererr").innerText=" ";
@@ -126,7 +133,6 @@ else if((female)){
 else{
     document.getElementById("gendererr").innerHTML ="please select any one";
 
-    document.getElementById("gendererr").style.color = "red";
     return false;
 }
 
